529084d4ca76be979fb15ff5803887bfdfa75ecb
乔书军 <shujun.qiao@chukong-inc.com>
2014-07-16 11:09:44 +0800

    add js sample.

---
+++ frameworks/js-bindings/cocos2d-x/cocos/platform/android/java/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+/workspace/git/cocos2d/anysdk-sample/js/frameworks/js-bindings/cocos2d-x/cocos/platform/android/java/bin/AndroidManifest.xml : \
+/workspace/git/cocos2d/anysdk-sample/js/frameworks/js-bindings/cocos2d-x/cocos/platform/android/java/AndroidManifest.xml \
---
+++ frameworks/js-bindings/cocos2d-x/cocos/platform/android/java/bin/classes/R.java.d
@@ -0,0 +1,1 @@
+ : /workspace/git/cocos2d/anysdk-sample/js/frameworks/js-bindings/cocos2d-x/cocos/platform/android/java/bin/AndroidManifest.xml \
